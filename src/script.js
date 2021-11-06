b9d52efa8f1ffb025a91cc75bcb473da1f39d439
robinsahu <sahurobin1497@gmail.com>
2021-11-07 00:10:45 +0530

    use statis folder to render images using parcel

--- src/script.js
+++ src/script.js
@@ -50,7 +50,7 @@ btnRoll.addEventListener("click", function () {
 
     // 2. Display dice
     diceEl.classList.remove("hidden");
-    diceEl.src = `./static/images/dice-${dice}.png`;
+    diceEl.src = `./static/assets/dice-${dice}.png`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
